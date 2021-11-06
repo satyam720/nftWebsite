486a98a3527cce969e6ad3ab3883508f46c9c250
satyam720 <psatyam1618@gmail.com>
2021-11-06 13:52:05 +0530

    added styled component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,21 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import NavBar from '../container/LandingPage/Navbar'
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    // <div className={styles.container}>
+    <>
       <Head>
         <title>NFT Website</title>
         <meta name="description" content="Get you own NFTs" />
         
       </Head>
 
-      
-    </div>
+      <NavBar />
+
+      </>
+    // </div>
   )
 }
